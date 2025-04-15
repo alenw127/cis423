5089ecd07fe4fe20544c1956f2734304b4439cfc
alenw127 <157432066+alenw127@users.noreply.github.com>
2025-04-15 14:38:24 -0700

    Update library.py

--- library.py
+++ library.py
@@ -5,5 +5,5 @@ import types
 from typing import Dict, Any, Optional, Union, List, Set, Hashable, Literal, Tuple, Self, Iterable
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
-import sklearn
-sklearn.set_config(transform_output="pandas")  #says pass pandas tables through pipeline instead of numpy matrices
+from sklearn import set_config
+set_config(transform_output="pandas")  # forces built-in transformers to output df
