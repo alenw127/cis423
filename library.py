1dbfb13037cd0eb082acd9cff3e44e0d962cf9ac
alenw127 <157432066+alenw127@users.noreply.github.com>
2025-04-15 14:40:40 -0700

    Update library.py

--- library.py
+++ library.py
@@ -5,5 +5,5 @@ import types
 from typing import Dict, Any, Optional, Union, List, Set, Hashable, Literal, Tuple, Self, Iterable
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
-from sklearn import set_config
+from sklearn.config import set_config
 set_config(transform_output="pandas")  # forces built-in transformers to output df
