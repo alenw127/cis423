3d4fa62a65dc98e9e3b666a621b2d0ccd1aa96ff
alenw127 <157432066+alenw127@users.noreply.github.com>
2025-05-01 17:23:15 -0700

    Update library.py

--- library.py
+++ library.py
@@ -9,6 +9,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import KNNImputer
 from sklearn.neighbors import KNeighborsClassifier 
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
 from sklearn import set_config
 set_config(transform_output="pandas")  #forces built-in transformers to output df
 
